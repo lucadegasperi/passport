3ef41c3c5d79bfeb2b77ce4b3f8bf0fdb2a0ab0e
Luca Degasperi <luca@lucadegasperi.com>
2022-07-26 13:56:53 +0200

    fixes

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -30,6 +30,7 @@ class ClientFactory extends Factory
             'redirect' => $this->faker->url(),
             'personal_access_client' => false,
             'password_client' => false,
+            'device_client' => false,
             'revoked' => false,
         ]);
     }
